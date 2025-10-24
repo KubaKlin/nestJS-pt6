45561b7f7b57983a19639c6e2eadaa1941702322
Kuba Klin <poczta@kubaklin.pl>
2025-10-24 12:23:09 +0200

    feat: add Authentication module with service, controller, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import Joi from 'joi';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     ProductsModule,
+    AuthenticationModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
